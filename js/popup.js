1335eaf272c7d23fd3708f3720e61e65f8379da7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-29 15:56:55 +0000

    Deploy Maskell/smart-device to github.com/Maskell/smart-device.git:gh-pages

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,71 @@
-const headerButton=document.querySelector(".page-header__contacts-item--call-back a"),popup=document.querySelector(".popup"),closeButton=popup.querySelector(".popup__button--close"),form=popup.querySelector(".popup__form"),userName=popup.querySelector("#popup-user-name"),phone=popup.querySelector("#popup-user-phone-number"),message=popup.querySelector("#popup-user-question");let isStorageSupport=!0;const storage={},openPopup=()=>{popup.classList.add("popup--show"),document.body.classList.add("overlay")},closePopup=()=>{popup.classList.remove("popup--show"),document.body.classList.remove("overlay")};try{storage.name=localStorage.getItem("name"),storage.phone=localStorage.getItem("phone"),storage.message=localStorage.getItem("message")}catch(e){isStorageSupport=!1}headerButton.addEventListener("click",(e=>{e.preventDefault(),popup.classList.add("popup--show"),document.body.classList.add("overlay"),storage.name?(userName.value=storage.name,phone.value=storage.phone,message.value=storage.message,message.focus()):userName.focus()})),closeButton.addEventListener("click",(e=>{e.preventDefault(),closePopup()})),form.addEventListener("submit",(()=>{isStorageSupport&&(localStorage.setItem("name",userName.value),localStorage.setItem("phone",phone.value),localStorage.setItem("message",message.value))})),window.addEventListener("keydown",(e=>{"Escape"===e.code&&(e.preventDefault(),popup.classList.contains("popup--show")&&closePopup())})),popup.addEventListener("click",(e=>{e.target===popup&&closePopup()}));
+const headerButton = document.querySelector(
+  ".page-header__contacts-item--call-back a"
+);
+const popup = document.querySelector(".popup");
+const closeButton = popup.querySelector(".popup__button--close");
+const form = popup.querySelector(".popup__form");
+const userName = popup.querySelector("#popup-user-name");
+const phone = popup.querySelector("#popup-user-phone-number");
+const message = popup.querySelector("#popup-user-question");
+let isStorageSupport = true;
+const storage = {};
+
+const openPopup = () => {
+  popup.classList.add("popup--show");
+  document.body.classList.add("overlay");
+};
+
+const closePopup = () => {
+  popup.classList.remove("popup--show");
+  document.body.classList.remove("overlay");
+};
+
+try {
+  storage.name = localStorage.getItem("name");
+  storage.phone = localStorage.getItem("phone");
+  storage.message = localStorage.getItem("message");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+headerButton.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  openPopup();
+
+  if (storage.name) {
+    userName.value = storage.name;
+    phone.value = storage.phone;
+    message.value = storage.message;
+    message.focus();
+  } else {
+    userName.focus();
+  }
+});
+
+closeButton.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  closePopup();
+});
+
+form.addEventListener("submit", () => {
+  if (isStorageSupport) {
+    localStorage.setItem("name", userName.value);
+    localStorage.setItem("phone", phone.value);
+    localStorage.setItem("message", message.value);
+  }
+});
+
+window.addEventListener("keydown", (evt) => {
+  if (evt.code === "Escape") {
+    evt.preventDefault();
+    if (popup.classList.contains("popup--show")) {
+      closePopup();
+    }
+  }
+});
+
+popup.addEventListener("click", (evt) => {
+  if (evt.target === popup) {
+    closePopup();
+  }
+});
