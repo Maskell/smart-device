fb2df36670abcc688181829956323137c8f9f4fe
Maskell <maltazar025@gmail.com>
2021-02-02 18:55:06 +0300

    Выносит повторяющийся код в функции

--- build/js/main.js
+++ build/js/main.js
@@ -74,48 +74,35 @@
 
 (function () {
   const footerSection = document.querySelector(".page-footer__wrapper");
+  const accordeonSections = footerSection.querySelectorAll(".accordeon");
+  const expandSection = (sectionIndex) => {
+    if (sectionIndex >= 0 && sectionIndex < accordeonSections.length) {
+      const section = accordeonSections.item(sectionIndex);
+      section
+        .querySelector(".page-footer__accordeon-button")
+        .classList.add("page-footer__accordeon-button--js-collapse");
 
-  const sitemapSection = footerSection.querySelector(
-    ".page-footer__sitemap-wrapper"
-  );
-  const sitemapList = sitemapSection.querySelector(
-    ".page-footer__list--sitemap"
-  );
-  const contactsSection = footerSection.querySelector(
-    ".page-footer__contacts-wrapper"
-  );
-  const contactsList = contactsSection.querySelector(
-    ".page-footer__list--contacts"
-  );
-  const sitemapButton = sitemapSection.querySelector(
-    ".page-footer__accordeon-button"
-  );
-  const contactsButton = contactsSection.querySelector(
-    ".page-footer__accordeon-button"
-  );
-
-  const setInitialState = () => {
-    sitemapButton.classList.add("page-footer__accordeon-button--js-shown");
-    contactsButton.classList.add(
-      "page-footer__accordeon-button--js-shown",
-      "page-footer__accordeon-button--js-collapse"
-    );
-    sitemapList.classList.add("page-footer__list--js-hidden");
-  };
-  const toggleSitemapSection = () => {
-    sitemapList.classList.toggle("page-footer__list--js-hidden");
-    sitemapButton.classList.toggle(
-      "page-footer__accordeon-button--js-collapse"
-    );
-  };
-  const toggleContactsSection = () => {
-    contactsList.classList.toggle("page-footer__list--js-hidden");
-    contactsButton.classList.toggle(
-      "page-footer__accordeon-button--js-collapse"
-    );
+      section
+        .querySelector(".page-footer__list")
+        .classList.remove("page-footer__list--js-hidden");
+    }
   };
 
-  setInitialState();
-  sitemapButton.addEventListener("click", toggleSitemapSection);
-  contactsButton.addEventListener("click", toggleContactsSection);
+  accordeonSections.forEach((section) => {
+    const accordeonButton = section.querySelector(
+      ".page-footer__accordeon-button"
+    );
+    const accordeonList = section.querySelector(".page-footer__list");
+    const toggleSection = () => {
+      accordeonButton.classList.toggle(
+        "page-footer__accordeon-button--js-collapse"
+      );
+      accordeonList.classList.toggle("page-footer__list--js-hidden");
+    };
+    accordeonButton.classList.add("page-footer__accordeon-button--js-shown");
+    accordeonList.classList.add("page-footer__list--js-hidden");
+    accordeonButton.addEventListener("click", toggleSection);
+  });
+
+  expandSection(1);
 })();
